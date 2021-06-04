f6a45b1fff2f27291cdeb78b52ebb6e2a5ce903d
Hung Dinh <hung.dnc@gmail.com>
2021-06-04 07:15:02 +0700

    From Mac@Home

--- src/store/store/domain/rules/user_email_must_be_unique_rule.py
+++ src/store/store/domain/rules/user_email_must_be_unique_rule.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import injector
+from injector import inject
+
 from foundation.business_rule import BusinessRuleBase
-from store import CountStoreOwnerByEmailQuery
+from store import CountStoreOwnerByEmailQuery, SqlCountStoreOwnerByEmailQuery
 
 
 class UserEmailMustBeUniqueRule(BusinessRuleBase):
-    def __init__(self, email, count_store_owner_by_email_query: CountStoreOwnerByEmailQuery):
+    @inject
+    def __init__(self, email, count_store_owner_by_email_query: CountStoreOwnerByEmailQuery = None):
         message = self.__class__.__name__
         super(UserEmailMustBeUniqueRule, self).__init__(message=message)
 
         self.email = email
         self.q = count_store_owner_by_email_query
+        if not self.q:
+            inj = injector.Injector()
+            self.q = inj.get(SqlCountStoreOwnerByEmailQuery)
 
     def is_broken(self) -> bool:
         return self.q.query(email=self.email) > 0
