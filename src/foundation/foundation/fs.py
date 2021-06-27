4df8dfc507a40d11e0a8b17b1b7137b352af1250
Hung Dinh <hung.dnc@gmail.com>
2021-06-27 19:53:52 +0700

    Change in gitignore

--- src/foundation/foundation/fs.py
+++ src/foundation/foundation/fs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import abc
 import json
 import os
 
@@ -9,35 +8,36 @@ from minio.helpers import ObjectWriteResult
 from werkzeug.datastructures import FileStorage
 
 
+def default_bucket_policy(bucket_name: str):
+    return {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {"AWS": "*"},
+                "Action": ["s3:GetBucketLocation", "s3:ListBucket"],
+                "Resource": f"arn:aws:s3:::{bucket_name}",
+            },
+            {
+                "Effect": "Allow",
+                "Principal": {"AWS": "*"},
+                "Action": "s3:GetObject",
+                "Resource": f"arn:aws:s3:::{bucket_name}/*",
+            },
+        ],
+    }
+
+
 class FileSystem:
     def __init__(self, minio: Minio):
         self._minio = minio
-
-    def default_bucket_policy(self, bucket_name: str):
-        return {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Effect": "Allow",
-                    "Principal": {"AWS": "*"},
-                    "Action": ["s3:GetBucketLocation", "s3:ListBucket"],
-                    "Resource": f"arn:aws:s3:::{bucket_name}",
-                },
-                {
-                    "Effect": "Allow",
-                    "Principal": {"AWS": "*"},
-                    "Action": "s3:GetObject",
-                    "Resource": f"arn:aws:s3:::{bucket_name}/*",
-                },
-            ],
-        }
 
     def upload_file(self, file_name: str, bucket: str, file: FileStorage) -> ObjectWriteResult:
         try:
             if not self._minio.bucket_exists(bucket_name=bucket):
                 self._minio.make_bucket(bucket_name=bucket)
                 self._minio.set_bucket_policy(bucket_name=bucket,
-                                              policy=json.dumps(self.default_bucket_policy(bucket_name=bucket)))
+                                              policy=json.dumps(default_bucket_policy(bucket_name=bucket)))
 
             file_size = os.fstat(file.fileno()).st_size
 
