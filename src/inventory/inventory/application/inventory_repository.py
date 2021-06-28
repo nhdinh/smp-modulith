83e9f3d834a29a9aebecc6e54fa6dca8fccb04d6
Hung Dinh <hung.dnc@gmail.com>
2021-06-28 17:32:55 +0700

    From Windows@Office

---
+++ src/inventory/inventory/application/inventory_repository.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import Union
+
+from foundation.entity import Entity
+from foundation.events import EventMixin
+from foundation.repository import AbstractRepository
+
+
+class AbstractInventoryRepository(AbstractRepository):
+    def _save(self, model: Union[EventMixin, Entity]):
+        pass
+
+
+class SqlAlchemyInventoryRepository(AbstractInventoryRepository):
+    ...
