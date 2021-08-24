aa7ed1efc7a09fa4b4fd8cd3500a97a584233c48
Hung Dinh <hung.dnc@gmail.com>
2021-08-24 11:54:31 +0700

    From Windows

--- src/shop/shop/domain/dtos/shop_brand_dtos.py
+++ src/shop/shop/domain/dtos/shop_brand_dtos.py
@@ -6,7 +6,7 @@ from typing import Union
 
 from sqlalchemy.engine.row import RowProxy
 
-from shop.domain.entities.value_objects import ShopBrandId
+from shop.domain.entities.value_objects import ShopBrandId, GenericShopItemStatus
 
 
 @dataclass
@@ -14,31 +14,36 @@ class ShopBrandCompactedDto:
     brand_id: ShopBrandId
     brand_name: str
     logo: str
+    status: GenericShopItemStatus
+    product_count: int
+    created_at: datetime
+    updated_at: datetime
 
     def serialize(self):
         return self.__dict__
 
 
 @dataclass
-class StoreProductBrandDto(ShopBrandCompactedDto):
+class StoreProductBrandDto:
+    brand_id: ShopBrandId
+    brand_name: str
+    logo: str
+    status: GenericShopItemStatus
+    product_count: int
     created_at: datetime
     updated_at: datetime
 
+    def serialize(self):
+        return self.__dict__
 
-def _row_to_brand_dto(
-        row: RowProxy, compacted: bool = True
-) -> Union[ShopBrandCompactedDto, StoreProductBrandDto]:
-    if compacted:
-        return ShopBrandCompactedDto(
-            brand_id=row.brand_id,
-            brand_name=row.brand_name,
-            logo=row.logo,
-        )
-    else:
-        return StoreProductBrandDto(
-            brand_id=row.brand_id,
-            brand_name=row.brand_name,
-            logo=row.logo,
-            created_at=row.created_at,
-            updated_at=row.updated_at,
-        )
+
+def _row_to_brand_dto(row: RowProxy) -> Union[ShopBrandCompactedDto, StoreProductBrandDto]:
+    return StoreProductBrandDto(
+        brand_id=row.brand_id,
+        brand_name=row.brand_name,
+        logo=row.logo,
+        status=row.status,
+        product_count=row.product_count,
+        created_at=row.created_at,
+        updated_at=row.updated_at,
+    )
